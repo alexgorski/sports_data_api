722559d44eb88c0d9a80ec0f1ffaeadfdc79cc8b
Alex Gorski <alex.gorski@gmail.com>
2013-06-05 07:21:47 -0400

    first draft of mlb team_spec.rb

---
+++ spec/lib/sports_data_api/mlb/team_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe SportsDataApi::Mlb::Team, vcr: {
+    cassette_name: 'sports_data_api_mlb_team',
+    record: :new_episodes,
+    match_requests_on: [:host, :path]
+} do
+  let(:team_rosters) do
+    SportsDataApi.mlb_key = api_key
+    SportsDataApi.access_level = 't'
+    SportsDataApi::Mlb.team_rosters(2013)
+  end
+  describe 'home team' do
+    subject { team_rosters.teams.first }
+    it { should be_an_instance_of(SportsDataApi::Mlb::Team) }
+    its(:id) { should eq '' }
+    its(:abbr) { should eq '' }
+    its(:name) { should eq '' }
+    its(:market) { should eq '' }
+    its(:league) { should eq '' }
+    its(:division) { should eq '' }
+    its(:venue) { should eq '' }
+    its(:players) { should have_at_least(9).players } 
+  end
+end
